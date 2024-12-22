0fa38d339ad33ae7346b8b5ce52cddb9b6be20dc
gedjerone <faxtro@icloud.com>
2024-12-22 07:26:17 +0300

    feat: Added neovim config

---
+++ lua/plugins/tokyodark.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "tiagovla/tokyodark.nvim",
+    opts = {},
+    config = function(_, opts)
+      require("tokyodark").setup(opts)
+      vim.cmd [[colorscheme tokyodark]]
+    end,
+  },
+}
